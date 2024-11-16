a5f707c34f59482d6b5343760248bbbf603caf67
anindyabanerjee <anindya.banerjee100@yahoo.co.in>
2024-11-16 08:51:26 +0530

    V1.0

--- src/RecordUsage.java
+++ src/RecordUsage.java
@@ -1,7 +1,7 @@
 public class RecordUsage {
     public static void main(String[] args) {
-        Student student1=new Student("John Doe",20);
-        Student.email ="abc@gmail.com";
-        System.out.println(Student.email);
+        Student student1=new Student("John Doe",20, new Student.Contact("eastvillage@gmail.com","1234567890"));
+        //Student.email ="abc@gmail.com";
+        //System.out.println(Student.email);
     }
 }
