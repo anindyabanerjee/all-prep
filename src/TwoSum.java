d383bf10bfd599ba3f2e3e02d309d7f0670da11f
anindyabanerjee <anindya.banerjee100@yahoo.co.in>
2024-12-10 02:25:08 +0530

    TwoSum.java

---
+++ src/TwoSum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+    }
+        public int [] sumOfNumbers(int [] argumentArray, int targetSum){
+            int [] result=new int[2];
+
+            Arrays.sort(argumentArray);
+
+            int i,midIndex;
+            for (i=0; i< argumentArray.length;i++ ){
+
+
+            }
+
+            return result;
+        }
+}
